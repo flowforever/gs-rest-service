99077edb656cdfa786b6519053cea1d9ec3853ff
Trump <trump.wang>
2016-07-14 15:36:15 +0800

    try field and property

--- complete/src/main/kotlin/world/WorldController.kt
+++ complete/src/main/kotlin/world/WorldController.kt
@@ -4,10 +4,28 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
+interface IController {
+    var goodStr: String;
+
+    @RequestMapping("good")
+    fun good(): String {
+        return goodStr
+    };
+}
+
 @RestController
 @RequestMapping("kotlin")
-class WorldController {
-    data class TalkContent(val title:String, val date: String);
+class WorldController : IController {
+
+
+    override var goodStr: String = "";
+        get() = field
+        set(value) {
+            field = value
+        }
+
+
+    data class TalkContent(val title: String, val date: String);
 
     @RequestMapping("/greeting")
     fun greeting(): TalkContent {
