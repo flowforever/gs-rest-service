2c08547f42e7dc04d3a8a04c9355a5f600e04d2a
Trump <trump.wang>
2016-07-15 10:46:32 +0800

    play some reflect

--- complete/src/main/kotlin/world/WorldController.kt
+++ complete/src/main/kotlin/world/WorldController.kt
@@ -1,8 +1,11 @@
 package world
 
+
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
 import java.util.*
+
 
 interface IController {
     var goodStr: String;
@@ -31,5 +34,39 @@ class WorldController : IController {
     @RequestMapping("/greeting")
     fun greeting(): TalkContent {
         return TalkContent("Hello", Date().toString())
+    }
+
+    @RequestMapping("/reflect")
+    fun reflect(): MutableList<String> {
+        //val cl = Thread.currentThread().contextClassLoader;
+        //val packageElements = cl.getResources("world");
+
+        //val dirFiles = packageElements.toList().map { File(it.file) };
+        //val classes = dirFiles.map { findClasses(it, "world") };
+
+        val list = mutableListOf<String>();
+
+        return list;
+    }
+
+    /**
+     * Recursive method used to find all classes in a given directory and subdirs.
+     *
+     * @param directory   The base directory
+     * @param packageName The package name for classes found inside the base directory
+     * @return The classes
+     * @throws ClassNotFoundException
+     */
+    fun findClasses(directory: File, packageName: String): MutableList<Any> {
+
+        val list = mutableListOf<Any>({})
+        for (file in directory.listFiles()) {
+            if (file.isDirectory()) {
+                list.addAll(findClasses(file, packageName + "." + file.name));
+            } else if (file.name.endsWith(".class")) {
+                list.add(Class.forName(packageName + '.' + file.name.substring(0, file.name.length - 6)))
+            }
+        }
+        return list;
     }
 }
